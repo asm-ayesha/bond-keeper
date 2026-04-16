bda40b9b504c8b864518ea8bcd27b9d0be4c2ca8
asm-ayesha <ayesha219333@gmail.com>
2026-04-16 23:21:54 +0600

    Remove unused prop from FriendCard component in friends page

--- src/app/friends/page.jsx
+++ src/app/friends/page.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const page = () => {
     return (
         <div>
-            <FriendCard from={friends}></FriendCard>
+            <FriendCard></FriendCard>
         </div>
     );
 };
